3aa535cb3e918ef0977974311ee344097afe0ece
Sui <bygreencn@gmail.com>
2014-07-27 21:14:05 +0800

    Xcode 5.1.1 compatibility

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ BUILD_CONFIG="-configuration Release"
 
 rm -rf "lib"
 mkdir -p "lib"
-xcodebuild -project libjpeg-turbo-ios.xcodeproj $BUILD_CONFIG -target turbojpeg $SDK_OS build | xcpretty -c || exit ${PIPESTATUS[0]}
-xcodebuild -project libjpeg-turbo-simulator.xcodeproj $BUILD_CONFIG -target turbojpeg $SDK_SIM build | xcpretty -c || exit ${PIPESTATUS[0]}
+xcodebuild -project libjpeg-turbo-ios.xcodeproj $BUILD_CONFIG -target turbojpeg $SDK_OS build || exit ${PIPESTATUS[0]}
+xcodebuild -project libjpeg-turbo-simulator.xcodeproj $BUILD_CONFIG -target turbojpeg $SDK_SIM build || exit ${PIPESTATUS[0]}
 
 lipo -output "lib/libturbojpeg-universal.a" -create "lib/libturbojpeg-arm.a" "lib/libturbojpeg-simulator.a"
 xcrun -sdk iphoneos lipo -info "lib/libturbojpeg-universal.a"
